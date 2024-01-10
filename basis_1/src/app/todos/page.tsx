7464780d53e84e7a4fae2ed157538b10491bf9d0
liceyoung <thachthao.8401@gmail.com>
2024-01-10 14:29:40 -0600

    update_2

--- basis_1/src/app/todos/page.tsx
+++ basis_1/src/app/todos/page.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+//using Usestate to change variable state
+// State is component's memory 
+// this component should remember something (**)
+
 export default function Todos() {
-        return ( // Named function: Todos # Anonymous function let parameter to not change the variable )
-            <div> Todos </div>
-        )
+// Named function: Todos # Anonymous function let parameter to not change the variable )
+    const [inputText, setInputText] = useState("");
+    // this coponent will remember inputText
+    // inputText: state variable which value I store 
+    // setInputText: setter function updating value of property in an object/ managing state and trigger React to render the component again. 
+    // array destructing [A, B]: unpack array into variables ( A then B in oderly)
+    // Most used data structure in Javascript are Object, Array (gather data items into an ordered list)
+    // array return by useState always has exactly two items. 
+    return (
+        // div : generic containter
+         <div className ="">
+        < div className="text-2xl"> Todo List Next </div>
+            <div className="">
+                <input 
+                type="text" 
+                placeholder="Enter Todo list" 
+                value={inputText}
+                onChange={(e)  => setInputText(e.target.value)} 
+                />
+            </div>
+        </div>
+    );
 }
