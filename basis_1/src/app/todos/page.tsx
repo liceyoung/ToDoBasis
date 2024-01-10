3a8344151ae2ac5f9d497c9a9a189f1fe7e1260f
liceyoung <thachthao.8401@gmail.com>
2024-01-10 12:50:13 -0600

    my changes

---
+++ basis_1/src/app/todos/page.tsx
@@ -0,0 +1,5 @@
+export default function Todos() {
+        return ( // Named function: Todos # Anonymous function let parameter to not change the variable )
+            <div> Todos </div>
+        )
+}
